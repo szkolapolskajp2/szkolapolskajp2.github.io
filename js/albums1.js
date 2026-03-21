765ce903a90ebc39f56c0dc16a8c3a0cf6ad8fe5
KD <konrad@addres.com>
2026-03-21 10:11:13 -0400

    updates

--- js/albums1.js
+++ js/albums1.js
@@ -1,138 +1,33 @@
-// auto-generated please update albums.txt
+// auto-generated please update albums{num}.txt
 var albums1 = [
-  [
-    "2025-2026/0. Stroudfest",
-    "AF1QipNZVg0jQGXwaLx0Uc-MX1TvgqqHappKhj9_716mRN5x3dhxxsRzMLlM4qbxzcobUw?key=cER1QVdJQWNKQWR1YWxBWmZvc0JSOHRUSmFNZEZn",
-    "AP1GczM3jzgHzzQFid-dq4C027-MlRyU_v_1R3JBAoY_J2GvXs_kHmvovn_okGjkiykw9DFY0bGRBxs8bmOE4pTnoSgjE4h_xNXeKYEemeRgzsa5QWfTlpbq=w600-h315-p-k",
-  ],
-  [
-    "2025-2026/1.Rozpoczęcie roku szkolnego",
-    "AF1QipN9S--gAfkO303QM1HsTGs4a0dIjdEmNogPkqTt6UhQZps8ZaKwlzM3nuvZJaOu5w?key=WlhBYmNEeEg4aE0zeXNzRHNiVDRTSmdhVHhtVU53",
-    "AP1GczPpZl1Yekbc5OhXitp04YuOof6GoTGsex-1qypJZrpQgA3S6kxHNANxcMWFMKQpAWHJlnC4SfT6K6C3Oyk2NSipi-Fx39wJd1Z-lKAaAAPa2H_14Q7Q=w600-h315-p-k",
-  ],
-  [
-    "2025-2026/2. Narodowe czytanie",
-    "AF1QipPbTYzPw4JlUrksjsJSLdqhd40phxoHVSrIB599rpWlNCKAiH3TdTOsCbRRnISZ3A?key=NnktZVZGbkd2WmV1RnpXTW4zQW95SDd3UU5VUmJ3",
-    "AP1GczN5IvD6EkBE8MzzeFPPO_gBsx8urzM0dUZs1EDtLNbTvhJRPubRNK-5PsW4u9LIsXyrBQSRz_ogjOtnBGxeaVjp1tCrrfZ_sZ833hP_dwAmtYMjFnx3=w600-h315-p-k",
-  ],
-  [
-    "2025-2026/3. Dzień przedszkolaka",
-    "AF1QipOAYaVFKL6Za2FHmCy7KdKvDY9Dw4nxiOElxoZ3gRnU5yd7b8HT9yB0OHcAXjjwYw?key=V0Nmc2pGZ3Bib0N3dTJJaU1oNGktRXBkbHU0bHhB",
-    "AP1GczNbUBNklNpNSNQ8Vx0_SmPzrex2SNPk-pFt3ez-ClNpj6YjKsuH70CCKE-v9_NVUVzb_A_ushiGtfSQS5vhLgR_80xmJEYFjoBV7yU0KVMPB1TSy2IA=w600-h315-p-k",
-  ],
-  [
-    "2025-2026/4. Książki z IRJP",
-    "AF1QipMO-dVzWzJQoVwYXNzDQnxEqhrKsA-E7ZyYZ5fAf7eNpnp6z-PwYqjfW19V-jb7zA?key=UDR2QVExa0s1MjJSdkVuTFptcjNILXZVVGR3Smxn",
-    "AP1GczPWJi_8aiiH81wsNQEsAOQy1fteGDfllgybHhnhz5ISg-ebhhc8I-xUGeAwT7NT8QGEUvB04T5sXvrRPNUgyg-5lkz7LNponiVjrswAGCPuQ2ZMoe66=w600-h315-p-k",
-  ],
-  [
-    "2025-2026/5. Parada Pułaskiego w Filadelfii",
-    "AF1QipN4CHSoBcjgA_8Tq20Iqi7tPcPHuF8WTL9FGhBtVn60fV7u7WVYoP_dR5ew992gbA?key=eXh2d0k4MjFERktNZ1QycGxyd1FieHVFTmZxMXNB",
-    "AP1GczOaENkw4olw6RDPU4emg31C14kjcCqV-Dt4A9jWfq0YXoOaICPzf_dJ0Bw5vH2XP9qAJI6astL1lDoQnMirlN--GxJUxb4zJbY6sBxzMcS5HUNJRpUS=w600-h315-p-k",
-  ],
-  [
-    "2025-2026/6. Jesień w klasie 1",
-    "AF1QipO0dX77MS1bdd4wZa6ngjxlQf9VbtdOj-T5tVDdBFefYasCGHBtowCSqMPT54qJqQ?key=cDBzbm1aZnFpNnROeXZnTTBKUTlFVmxMVVZmMXZn",
-    "AP1GczOsE5szycgE2D2XDtJOIbTouLAcg0h3kygbgupSete-9lNuadrrzVJ6S1WpI6zEfyoNZ23fdAnry0R-TGBldWRK69SpGR3HXac3gEbo5rLmPS8sNmcA=w600-h315-p-k",
-  ],
-  [
-    "2025-2026/7. Wycieczka do Waszyngtonu",
-    "AF1QipN2nujOJLm7JoJUwZ7YtR5kXKx5iS3tK-Kkp8GyxukWsOsse44zBjYCM96xfn0NKg?key=UGpoaGR6c0hjdE5YTnozSGxYb3l1SEduVkVtVTlR",
-    "AP1GczOJCwpCQdIDK7PkqSI2m-4tGyskf6CpHjEKqDIGCyom4OeQawij7fw77aCyqtxudmXrKE2jIDw1Wn8aUczN9og-eilQ3WPlKgTOiWLk6U1lzeT48Aa6=w600-h315-p-k",
-  ],
-  [
-    "2025-2026/8. Warsztaty uważności",
-    "AF1QipMXcHa8C1bKG8POugG_ThPe4RwOXLhF0UYFIXuxWLF-3c-Fo_zy5QR3XJ-SGxfLEg?key=NUJXVTRUZTF4R0RtVl94YmxwTjdWMHh5OVFLVDFB",
-    "AP1GczMyxWfUT5kx6n3yC1L8VqoBwFp4rOxX36ic5KIxk2g0y_77einqXBYEjaQBDGyIdTo6kkMcsGz3hRbtXVqrkWdG1k4_Gec8vRtmcjMDX2Me8FciMDrY=w600-h315-p-k",
-  ],
-  [
-    "2025-2026/9. Polonijny Dzień Dwujęzyczności",
-    "AF1QipMVMy15qAi2PyuFY4xo0NQyncAawAozjd-_gh7OvjXY4M1b5MGpBd2acbKbIVCRHA?key=aF9pTTJpRzFsUGVVc2l0cmFkZFVjOVkyQUk1ZEdB",
-    "AP1GczNMqaDf-CAY-BqxNTpMGxjS2b3KO9jnqAf9ljOZ-HpfEFr5jaIpjziCA3ducNeWmyL5a0dBbqIbWC-KY2DghS6xIAH56UcfjS9GmDqfHk_Hxyca5WdZ=w600-h315-p-k",
-  ],
-  [
-    "2025-2026/10. WLCE egzamin ustny",
-    "AF1QipMw5O-uOh8PI1yiykT1kZzv4GCaNylt5T9G8OwBCIDDhPUksHrCCx06ig7Of0zq5w?key=d3dmUS1HM0JZa0ZtTUZJQWdsZnM2RUtsT05vYVdR",
-    "AP1GczM4wuNpOABUTVhLoJ_x3iLrn2IemZXqyecFHf4HDC82-ijDnOvpOWhVLs_wCcnhlZD0hMSLM4fdYsSxwFxSVlwX87zTCHyfpRrzVw_BAZqd_0Uzina7=w600-h315-p-k",
-  ],
-  [
-    "2025-2026/11. Mazezilla piknik jesienny",
-    "AF1QipOuqigM7oXqvEi29UiXgJyoFBRKTJOIkMdLxwUJ8-XLvWj1x-pdF9Y1cQkmF7PeYQ?key=Q0M2OU9DOGsxQTc3Tm9IXy1TdzlLdThnS1V0Q2RR",
-    "AP1GczP3YyH9l7W2CBCxj_3CTxbCgCNq6BDvXKZi4t-xDcghGjwxkg7JvuTDEzyaiLOzYd2mYkPD9EBdHvLMXPJYUyxs1QNgXoUHW3h_RpCX1v4A-pZMkfY7=w600-h315-p-k",
-  ],
-  [
-    "2025-2026/12.Akcja &quot;Pamiętamy o weteranach&quot;",
-    "AF1QipMb9vuAASPmpvP_2MTCAgQ53DRNHYcsDc9oTbeStdj6xW3rG9nBarLzX6xScvjUUg?key=ZjVuV0NLM2RhNGpWaDBmdndNbkR3aGdwcHpTMlJn",
-    "AP1GczNc9-d8ru02yB1ddwbf2DKJB79BtoM_T89AcmYsJncEsv7LgzvZOiaNz1EJhPfTFWHxvvOf_crBzrLxU__GUh3ahdgckySXzWGcnY6qLAkNb-aTpRB0=w600-h315-p-k",
-  ],
-  [
-    "2025-2026/13.Debata 2025",
-    "AF1QipOtJgNp1ANLq8NqfcnuggiE-z6sfMcZWcgLJIUuUHonKM3RJ8E65JVjDMCAksg1LQ?key=bmY3b3FKYkowb2h3clFvS1BXUjQ4V3VpSWhTX0NB",
-    "AP1GczNWUqZh4fSkwRuLnnE2IlxCAVXlVj_a9BvxBmOOLp2xw-O7L4QAkLXrJFI-nWja73nc4Cto9Y2IY3EyW-9xPDaPEXgyoPwd2s1snOUpTAFYJQ8B4vZ8=w600-h315-p-k",
-  ],
-  [
-    "2025-2026/14. Święto Niepodległości w klasach",
-    "AF1QipO9u1S1e7ubUcZ2LxqKOiadjWjrNFaluJVJMO0W7qjoSu4kG6nZxws5GhkFJioUOQ?key=ZXBVN1dyWlZtVndZX3NQM1AzbVkyT0RXZWxQeVh3",
-    "AP1GczN-CY4KQsWtF6OQ_ybEn3wSxa6J1hjnB4A6TaCCkBYdfisGi0WTQEZgHR0RpFHFzyGwQkIqnhlzrUrISWCJANdaZhiaL4YRH9Q1z41t4BdEFdzo7RdP=w600-h315-p-k",
-  ],
-  [
-    "2025-2026/15. Apel z okazji Święta Niepodległości",
-    "AF1QipPLq1r_FHczclhvUbLSdzyLTKXb9V2tSos60FrYFD9b0QckXihswjW9ZM-DnlhKgw?key=elN3Y2wtNG96M2pOSjc1V01OSi10Y0xkT0pBNnhn",
-    "AP1GczP-FzspVUHJnCvk613gby67sve2E8TQNyLEfVWGrIyfDNgclWxX1i381YmRRrF33aJ3aDKXB8HzPp9pRe4RHrBo7VbN_aHZKGCpHLuGDgS07JVFAmbe=w600-h315-p-k",
-  ],
-  [
-    "2025-2026/16. Bal z okazji Jubileuszu 100-lecia CPSD",
-    "AF1QipNrJwdYG1iHHkAdqQv59V1RSj9xyx1buXuMaifAZnf9dtaryfSLrp6L-_nOvdrznQ?key=Y2ZuUTlralIxSk1GdU1NRVA2VVFILUdERUN5TTRB",
-    "AP1GczPmqQ3mCyzr2PUgZeSsPbvVUR4yAbi9tOhZc9TDAcFRShJr5EJYGfiop0GUT7DVMN6Xcu0IWnBs3T1m-q8f-Dq-vgEGyQOYTMiowm6ra2oIv9MLZzr5=w600-h315-p-k",
-  ],
-  [
-    "2025-2026/17. Andrzejki",
-    "AF1QipMzMq13SGUgMuhHVWuTVEGXyRtsCm20nHCGp_x7dfYdIAuxFwO_--V23i3jhMWK1Q?key=Y0JkcmRtUkVWb19Rc1RQNVF4N0h1T0pQYjlmVUN3",
-    "AP1GczOOROU5-mpobE0W8OJtH-PxLdu_hPpivE6Ybo6pRyHIPuP1VcK8HVytTxMN_rTl-2ymTZP5cX7t41adj4-km3zeB5M1ZIDBWFtlOpPawiVxxpsOaBug=w600-h315-p-k",
-  ],
-  [
-    "2025-2026/18. Mikołajki",
-    "AF1QipPEp5KlhKS48fpqbih_zV7LsKJ5kMnhFtk-OOgySOoGxER8ZYVGYHQbQDMEqGAvCw?key=cnhnNVczQnctSWh1MTRsTGxrSkhFWGF3ZmZrTy1B",
-    "AP1GczNHC8-ILv41NidMHv-g2CcDbOtme3vlue1YPeX-J2weqpI7kygbBTbjNvHPBZWoSuAT0bE3M-R-ZLfnS6vu1BXa-3vlmX3Lb0ivx5ui_IKB9j4Oocic=w600-h315-p-k",
-  ],
-  [
-    "2025-2026/19. Dzieci-dzieciom #Kaszanka 8.0",
-    "AF1QipMkXnUOvTZhpl8IQLF2c3upeJGOY9UEZee27i0-FeaZHeXsFExnuTp7yAKYsdT_kA?key=QjFoU0x1aFRCaUhNOEVpaXYyYlg0b1N0QkJINFpn",
-    "AP1GczOyO8XB2KuJgps8I2CGrh8-AZDnX2AdNk7JRXE2rwfE-4_cmsFZynlMrp_gkfDiH2kKED88fPE_BYZVUw52qvy23GSvtMXV42lMDrOznNVuc0YEImFo=w600-h315-p-k",
-  ],
-  [
-    "2025-2026/20. Kiermasz Świąteczny &quot;Nasza Kraftoteka&quot;",
-    "AF1QipN_LA_pUmoNy9kGNdD_8C6jbtMAqTEUu0fwD_7476_2uKf08dLHoOg2Z0yOHEjoJQ?key=Sk5wLXFVbFJJSTF5UGtfSkdjWUV6VTVVRWt1VWR3",
-    "AP1GczMT9rCXmYe6Dgdyr9qQx7-Fb5z7trTxgF3PKC63mrtwo1LS6yHrdh5Ue7jW34nFGN7_LxU-axWD8ttQBChgM5RV6G2Eh_Fl6mVbwIYVN3zFUxHQzxvZ=w600-h315-p-k",
-  ],
-  [
-    "2025-2026/21. Spotkanie literackie, Ewa Chotomska",
-    "AF1QipMufwfWBIL566EonUWDsS7dMgWEXbByfnnvNL-xCNjzjBLyNYsws9Z0xbcjGuQ36Q?key=cGlQRXlhNDFlRnctVkotVjBZTk11VjNiVU9idmZB",
-    "AP1GczNWEMQgMv8IzvTkbYdkA1FpTcSZkafh1WbKYBg4Fx1jyrBTcCdd958mwXrrnct9QITmoisoer6VGfoLxjrm8HCDmeCqm5T5jxDeYLM3WbxIdt0S5WOh=w600-h315-p-k",
-  ],
-  [
-    "2025-2026/22. Wigilie klasowe",
-    "AF1QipOECV-uGxmYxNVdeQAAwKi-b2pHs1DeGR8Yog9jr_-h-HeETibl2JqX0JmGUQ4T6g?key=clVhU3dxMDNNVjBTNG1TSEkxVUpSODN4d2xsQlJR",
-    "AP1GczOzf-Jpl3yirz7ZPaISn9o1HD0PrmkySIpfWcVQRs8E_ir61bWKWSJcmxMiAZDex7a3hdjg2MPqqI0Bn0RPyp7HxMDG2g0k3I2z1yktIC4m4VmxLK50=w600-h315-p-k",
-  ],
-  [
-    "2025-2026/23. Jasełka 2025",
-    "AF1QipM_VYM1SNdAjX9XaEO8gm2kKq46z78SKAEz5UGHcGn2DgzgGn3gbkAO-vKj5LXrvA?key=S2VRUkRqcVFHaThsU01LckF1UE0ydnZEdmNESUl3",
-    "AP1GczPq3jnCHMzkFYSIArXRDZfidDp9fDU21RWTNdY4oO8ZJjlsgP8PxUialuQq5MDlGmUA3xXjKlARFl-ma2J4bzd3SHZ6JXe3s0axe-20eInvHOxKs6vX=w600-h315-p-k",
-  ],
-  [
-    "2025-2026/24. Ślubowanie i Pasowanie na Ucznia",
-    "AF1QipO4ePC7fWKemmkdIiHoxRm2GbSx65R4wpicbF6z3BPvRQEAkvWQfs_xfYA-b2kC3g?key=UGVidFA1MzNCNi1KUlF0VUdYTkZYWWdpdnA4a0xn",
-    "AP1GczPX1LywE80wQwrJLY6qayw6DXbOgZ4Ec5Hn6kCGh3Ej5Vd-K6H2pIgAOgG7krZEM0wQWklzf_4zSYQPi3Yjv4sciCodjbOgdFva4kr9DZbsAS9kwJhq=w600-h315-p-k",
-  ],
+["2025-2026/25. Medale KEN",
+"AF1QipMom7s6ex_-tELxECrMD0ingRvV--GuHP01_ccpUOHVmSWfP6aAmYcQLIYIlHMy_g?key=cmt6RzNRTkRJUWNTczJIOGxCTE9ZNk4zRGdwSUlB",
+"AP1GczMvOUH2-2gYUi2lV9orgCf4tp1-JeRSXn53ZkLbhlnCqhc6lLVkbBinn_zvL6SFJY78nEDB5JgecIaH3ZLIO37tHXpTjgSHlTuJZrDnm5GuEfh5da5F=w600-h315-p-k"],
+["2025-2026/26. Zabawa Karnawałowa na rolkach i wrotkach (klasy starsze)",
+"AF1QipORFAer5_MfOM5AHW9Np6vhq8nXEPSCZVJ-RHkC4cKmVgrZc0a8Oj2EYhGbqjZH8A?key=WFJwMmZfRWNNc19Ncm9QYTA1TTJ0ejBFWjlrNThB",
+"AP1GczMRRo0yibDiax95jg3-pwUUjqiYoC9Er4qx7135sNBtIirY9ZN2zUaxqx9NSehYiIwFRGhvwaGsZMHcpkpROOSniJHnRJDJIKUMzKvvj1dAnmJae79f=w600-h315-p-k"],
+["2025-2026/27. Zabawa Karnawałowa na rolkachach i wrotkach (klasy młodsze)",
+"AF1QipMWogDtxUXNLNWg7t2RdL65lk0Gp8F59Z4xpfpwLWNx0WYI5vpx-AQZZzR2waToMw?key=N1RFeHV6a0w0Nnc2Y0pLNXZqWnVzNkxHSFk3TEJn",
+"AP1GczNcxTu-QrmEDlpXddSgpTizwHX8EAfzfzI-zH1KHrenJDgbFAqAxWqU0gJA1zzK9eBWJCMZJ-Iqu9qX2D9AP5o4UMHxzgkmNJSj53E9EuwPWBTJJwl5=w600-h315-p-k"],
+["2025-2026/28.WOŚP",
+"AF1QipO9dNrssdXnIc0uidiL2lNZ64gpr5cEFsHutnuxA0ZLeR46ybA0dy13Ci2Yiw5Feg?key=LXVtMmFtdENXN3BnWDlZWFdaRDNkMlNQYXRvUFZB",
+"AP1GczOUBnl-_xBBBxYHqmjH2ejyaQkKCLwSdOdkpIMWbBi75as1aRJcgXFZ1gmN4_H8e2MOFjGaydzH0KlhhJynPLEWAre4-m0_dPFEXDDtyfCI6gZ1PduN=w600-h315-p-k"],
+["2025-2026/29. Studniówka 2026",
+"AF1QipOlAqkG-V2xH1WMtVOW41cRADHA8m_sfrrkY8HzXSK4ggASsjFbQw1Tv-WbZYgaDA?key=bUx6RElxeXBxWHBkU2poUHB0MHo1X0xudTJRNXNn",
+"AP1GczNJVMxWCHKWuReF2TJITxIWE3kk6siG9HQi8aqnEPKFkz60B_80QuuKsm2yvOO7gMEdZrcHh8xQs-5esvtj8iS3sobg4TIJ_IdUqbE7QgHnfXcvDdJa=w600-h315-p-k"],
+["2025-2026/30. Walentynki-Zabawa ostatkowa",
+"AF1QipMwJViYUVdnnostGq5eJc2BAE5wHA6rAoM7w9rD5carC6zKoENgzZpDVvKBEdSKlQ?key=alllSEhnOWhvYzhXVGRrTFoyckdQcy1NRG44bnJR",
+"AP1GczPcn1C14tDxaG9NrR0Mo9PPrBJqy74bXLDCTGmG4d5-USBjk9icBaaYLnws447sGCsStw03B6YCGCy1FM_Rb0CDnzKedJ7F4cRWFHSfe-sfr5VE-3wn=w600-h315-p-k"],
+["2025-2026/31. Walentynki w klasie zerowej",
+"AF1QipN6k1YCvZVcHzQTMKOxiiVZNt4NsgXpOp3lq-uU5iLfjD-OomYy3MJfDCfCVAjicQ?key=c2V3WnU5UTIwYlFyQXJLNE92dGctX3ZwT1ZzRC13",
+"AP1GczN0XCGpagRw7CIorYeTnHB8GZOsmJsgJwokKYhw_0ljRzw_iTrq5skYl-a-m2pyOZ4ciS1r4XY1CFzbdfAbAc6h-_JN7D9gsvjdJYnBmfwUZjk6D7hv=w600-h315-p-k"],
 ].map(([title, albumUrl, coverUrl]) => {
-  const [year, index, name] = title.split(/\/( *[0-9]{1,2}\. *)/g);
-  const parsedEntry = [
-    name ? `${year}/${name}` : year,
-    "https://photos.google.com/share/" + albumUrl,
-    "https://lh3.googleusercontent.com/pw/" +
-      coverUrl.replace(/=.*$/g, "=s222-p-k-no"),
-  ];
-  parsedEntry["index"] = parseInt(index) + 1 || 100;
-  return parsedEntry;
-});
+    const [year, index, name] = title.split(/\/( *[0-9]{1,2}\. *)/g)
+    const parsedEntry = [
+      name ? `${year}/${name}` : year,
+      'https://photos.google.com/share/' + albumUrl,
+      'https://lh3.googleusercontent.com/pw/' + coverUrl.replace(/=.*$/g, '=s222-p-k-no')
+    ]
+    parsedEntry['index'] = parseInt(index) + 1 || 100;
+    return parsedEntry;
+})
