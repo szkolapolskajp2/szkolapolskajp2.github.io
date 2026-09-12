a1f8de8680d0a6f91f52583f2faf899ca7fe203e
KD <konrad@addres.com>
2026-09-11 23:05:21 -0400

    fix

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@
   $(".dotacja-carousel")
     .append(
       `
-      <img  class="img-responsive dotacje" src="/img/dotacje/dotacja2026.jpg"/>
+      <img  class="img-responsive dotacje" src="/img/dotacje/dotacja2026.png"/>
       <img  class="img-responsive dotacje" src="/img/dotacje/bon-dla-nauczyciela-2025.png"/>
       <img  class="img-responsive dotacje" src="/img/dotacje/bon-dla-ucznia-2025.png"/>
       <img  class="img-responsive dotacje" src="/img/dotacje/dotacja2025.jpg"/>
